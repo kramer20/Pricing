ac1dca2616abc4efde1b019243d48499b5e35abc
Malorie Kramer <maloriekramer@Malorie-Kramers-MacBook-Pro.local>
2016-06-06 15:24:24 -0400

    second commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
 var blueCart = 29;
 var pinkCart = 19;
 var orangeCart = 39;
-var tax = .065;
 var total = 0;
 
 function add29() {
 	total=total+blueCart;
   var input = document.getElementById("subtotal");
+  var totalBox = document.getElementById("total");
   input.value = total;
+  totalBox.value = calculateTotal();
 }
 
 function add19() {
 	total=total+pinkCart;
   var input = document.getElementById("subtotal");
+  var totalBox = document.getElementById("total");
   input.value = total;
+  totalBox.value = calculateTotal();
 }
 
 function add39() {
 	total=total+orangeCart;
   var input = document.getElementById("subtotal");
+  var totalBox = document.getElementById("total");
   input.value = total;
+  totalBox.value = calculateTotal();
 }
 
-
+function calculateTotal() {
+  return (total*.065) + total;
+}
